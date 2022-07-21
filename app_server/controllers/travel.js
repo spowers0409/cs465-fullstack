82b35b877fd62bcecd70db3c73070d4be3e3870d
spowers0409 <therealsjpowers04@gmail.com>
2022-07-21 08:18:02 -0500

    Create object schema with Mongoose, populate MongoDB

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const trips = JSON.parse(fs.readFileSync("./data/trips.json", "utf8"));
 
+// Get traveler view
 const travel = (req, res) => {
   pageTitle = process.env.npm_package_description + " - Travel";
   res.render("travel", { title: pageTitle, trips });

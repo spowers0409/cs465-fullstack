1bacbb52482b34fab408f65e8ee865a94c8c750f
spowers0409 <therealsjpowers04@gmail.com>
2022-08-04 23:05:01 -0500

    Added SPA with add and edit functions

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ require("./app_api/database/db");
 var indexRouter = require("./app_server/routes/index");
 var usersRouter = require("./app_server/routes/users");
 var travelRouter = require("./app_server/routes/travel");
+//var roomsRouter = require("./app_server/routes/rooms");
+//var newsRouter = require("./app_server/routes/news");
+//var mealsRouter = require("./app_server/routes/meals");
+//var contactRouter = require("./app_server/routes/contact");
+//var aboutRouter = require("./app_server/routes/about");
+
 const apiRouter = require("./app_api/routes/index");
 
 var app = express();
@@ -25,9 +31,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+// Allow CORS
+app.use("/api", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
+});
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/travel", travelRouter);
+//app.use("/rooms", roomsRouter);
+//app.use("/news", newsRouter);
+//app.use("/meals", mealsRouter);
+//app.use("/contact", contactRouter);
+//app.use("/about", aboutRouter);
+
 app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
